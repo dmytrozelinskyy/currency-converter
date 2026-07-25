d674ea1e9072cb622ff9d82d8f2623f49ba9adfa
dmytrozelinskyy <dmytrozelinskyy07@gmail.com>
2026-07-25 14:42:29 +0200

    feat(security): add HTTP Basic auth and permit H2 console access

---
+++ src/main/java/io/github/dmytrozelinskyy/currencyconverter/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package io.github.dmytrozelinskyy.currencyconverter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/cc-console/**"))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/cc-console/**").permitAll()
+                                .requestMatchers("/api/**").permitAll()
+                                .anyRequest().authenticated()
+                ).httpBasic(basic -> {});
+        return http.build();
+    }
+}
